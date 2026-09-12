6b427fb91870fe610689d08fffbb91edf16f56c7
Alex <bigbossalex11@gmail.com>
2026-09-12 03:29:13 +0200

    Skriv ut spelarlistan med map och key

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,13 @@ const [players, setPlayers]= useState ([
   {id: 3, name: "Oskar", rank: "röd"}
 ]);
   console.log(players)
-  
+
   return (
     <div>
       <h1>Lagindelning</h1>
+      <ul>
+        {players.map(player => <li key={player.id}>{player.name}, {player.rank}</li>)}
+      </ul>
     </div>
   );
 }
